4824fb2227fe6a7e602930684d1b800757d1a3e9
K4YT3X <k4yt3x@k4yt3x.com>
2021-04-27 09:29:34 +0000

    added basic logging functionalities

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,9 @@ Licensed under the GNU General Public License Version 3 (GNU GPL v3),
 (C) 2021 K4YT3X
 */
 
-use actix_web::{get, web, App as ActixApp, HttpResponse, HttpServer};
-use clap::{App, Arg};
+use actix_web::{get, web, HttpResponse, HttpServer};
+use clap::Arg;
+use log::{error, info, warn};
 use serde_json::Value;
 use std::fs;
 use std::result;
@@ -19,15 +20,23 @@ fn read_redirect_table(path: String) -> result::Result<String, Value> {
     // read the redirect table and return the path's corresponding destination URL
 
     // read JSON file into a string
-    let redirect_table = fs::read_to_string("akasio.json").unwrap();
+    let redirect_table = fs::read_to_string("akasio.json");
+    let redirect_table = match redirect_table {
+        Ok(value) => value,
+        Err(_e) => {
+            error!("Error reading the redirect table");
+            return Err(Value::Null);
+        }
+    };
 
     // parse JSON string
     let value = serde_json::from_str(&redirect_table);
-
-    // if the read operation failed, let value be Null
     let value = match value {
         Ok(value) => value,
-        Err(_e) => Value::Null,
+        Err(_e) => {
+            error!("Error reading the redirect table");
+            Value::Null
+        }
     };
 
     // create lookup string
@@ -39,6 +48,7 @@ fn read_redirect_table(path: String) -> result::Result<String, Value> {
 
     // if key is not found, return Null
     if target_url == &Value::Null {
+        warn!("Path {} was not found in the redirect table", lookup_string);
         return Err(Value::Null);
     }
 
@@ -55,6 +65,7 @@ async fn redirect(path: web::Path<String>) -> HttpResponse {
         Ok(destination) => destination,
         Err(_e) => {
             // if the read returned Null, return not found
+            info!("Path not found, serving 404");
             return HttpResponse::NotFound()
                 .content_type("text/plain")
                 .body("Not Found");
@@ -62,6 +73,7 @@ async fn redirect(path: web::Path<String>) -> HttpResponse {
     };
 
     // if the read succeeded, return 302 found
+    info!("Path found, serving 302");
     return HttpResponse::Found()
         .header("Location", destination)
         .finish();
@@ -72,7 +84,7 @@ async fn main() -> std::io::Result<()> {
     // program entry point
 
     // parse command line arguments
-    let matches = App::new("Akasio Redirector")
+    let matches = clap::App::new("Akasio Redirector")
         .version("0.1.0")
         .author("K4YT3X <k4yt3x@k4yt3x.com>")
         .about("A simple Rust program that redirects HTTP requests.")
@@ -85,19 +97,27 @@ async fn main() -> std::io::Result<()> {
                 .takes_value(true),
         )
         .arg(
-            Arg::with_name("debug")
-                .short("d")
-                .long("debug")
-                .help("enable debugging mode"),
+            Arg::with_name("redirect-table-path")
+                .short("r")
+                .long("redirect-table-path")
+                .value_name("REDIRECT_TABLE_PATH")
+                .help("path to the redirect table file")
+                .takes_value(true),
         )
         .get_matches();
 
     // read matches into variables
     let bind = matches.value_of("bind").unwrap_or("127.0.0.1:8000");
-    // let debug = matches.is_present("debug");
+    let _redirect_table_path = matches
+        .value_of("redirect-table-path")
+        .unwrap_or("akasio.json");
+
+    // initialize logging
+    env_logger::init();
+    info!("Starting Akasio (Rust) server");
 
     // start server
-    HttpServer::new(|| ActixApp::new().service(redirect))
+    HttpServer::new(|| actix_web::App::new().service(redirect))
         .bind(bind)?
         .run()
         .await
